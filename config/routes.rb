41239ef3346450c78b6a55d0efc97b9c32f6407f
Erik Nomitch <enomitch@gmail.com>
2014-12-26 10:23:03 -0500

    rails generate controller Landings index

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
+  get 'landings/index'
+
   #match "/font_awesome_cheatsheet.js" => "/assets/font_awesome_cheat_sheet.js"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'landings#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
