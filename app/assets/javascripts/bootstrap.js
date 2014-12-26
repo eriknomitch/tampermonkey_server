13d4075b9110e29ecc25d82959ee6a1132d9ee74
Erik Nomitch <enomitch@gmail.com>
2014-12-26 08:51:55 -0500

    + bootstrap.js

---
+++ app/assets/javascripts/bootstrap.js
@@ -0,0 +1,58 @@
+function GM_bootstrap(GM) {
+
+  GM.remote.urlNoProtocol = "//"+GM.remote.domain+GM.remote.suffix;
+  GM.remote.url           = "http:"+GM.remote.urlNoProtocol;
+  GM.remote.stylesheet    = GM.remote.url + "/" + GM.name.system + ".css";
+  GM.remote.script        = GM.remote.url + "/" + GM.name.system + ".js";
+
+  var div = document.createElement("div");
+
+  div.id = "GM_load_block";
+
+  var css = {
+    "position": "fixed",
+    "top": "0px",
+    "right": "0px",
+    "bottom": "0px",
+    "left": "0px",
+    "background": "#ffffff",
+    "z-index": "999999"
+  };
+
+  for (var prop in css) {
+    div.style[prop] = css[prop];
+  }
+
+  document.getElementsByTagName("html")[0].appendChild(div);
+
+  //console.log(GM_info);
+  //var GM_script_name = 
+  //unsafeWindow.GM_script_name = "
+
+  (function GM_initializeWhenjQueryReady() { 
+    if (unsafeWindow.$){
+      
+      $(document).ready(function() {
+        
+        // Inject Script
+        var s = document.createElement("script");
+        
+        s.type = "text/javascript";
+        s.src  = GM.remote.script;
+        
+        $("head").append(s);
+        
+        // Inject Stylesheet
+        $("head").append("<link rel=\"stylesheet\" href=\""+ GM.remote.stylesheet + "\" type=\"text/css\" />");
+        
+      });
+      
+      return true;
+    }
+    
+    setTimeout(GM_initializeWhenjQueryReady, 1);
+  })();
+
+  return GM;
+}
+
