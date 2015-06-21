4083aee6cd67466c698a3833a8ec450ade13ad4b
Erik Nomitch <erik@nomitch.com>
2015-06-21 16:12:44 -0500

    adds VERSION file with Application class reading from it

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,4 +1,5 @@
 class LandingsController < ApplicationController
   def index
+    render text: "Ubermonkey Server v. #{UbermonkeyServer::Application::VERSION}"
   end
 end
---
+++ config/initializers/version.rb
@@ -0,0 +1,6 @@
+module UbermonkeyServer
+  class Application
+    VERSION = File.read("VERSION").chomp("\n")
+  end
+end
+
